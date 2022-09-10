a490e9dc051e66c39142239688a3c902d4cae011
John Mwagiru <84149698+john-the1-mwagiru@users.noreply.github.com>
2022-09-10 14:16:51 +0300

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - return integer
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+putchar('\n');
+return (0);
+}
+Footer
+© 2022 GitHub, Inc.
