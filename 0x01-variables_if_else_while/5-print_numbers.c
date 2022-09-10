2bce11e0d36be58d8d96cab6646c8aff917e4bf2
John Mwagiru <84149698+john-the1-mwagiru@users.noreply.github.com>
2022-09-10 14:09:09 +0300

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - return single digit
+ * description: 'Return numbers from zero to nine
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+printf("%d", num);
+printf("\n");
+return (0);
+}
