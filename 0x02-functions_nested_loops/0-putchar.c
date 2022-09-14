6ef57c8f35096674fc183492687996d61f4e04c2
John Mwagiru <84149698+john-the1-mwagiru@users.noreply.github.com>
2022-09-14 21:48:45 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,19 @@
-#include <stdio.h>
-/* 
- * main - entry point
- *
- * Return : a word to the display
- *
- * */
-
-char main (void) {
-
-	printf("_putchar \n");
-
-	return (0);
+#include "main.h"
+/**
+ * main - Prints "_putchar" followed by a new line.
+ * Return: Always 0.
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
 
