2bd26df98719b2187a8c7737ef0f67f26bbd3dbe
John Mwagiru <84149698+john-the1-mwagiru@users.noreply.github.com>
2022-09-20 00:01:02 +0300

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+/*
+ * File: 2-strlen.c
+ */
+
+#include "main.h"
+
+/**
+ * 2-strlen.c - returns the length of a string.
+ * @s: character to be rested.
+ */
+int _strlen(char *s)
+{
+strlen(*s)
+}
+ 
