fcd1284dd4a47eb60f14ea780e7cb70dee00e8ca
John Mwagiru <84149698+john-the1-mwagiru@users.noreply.github.com>
2022-09-20 00:38:12 +0300

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+/*
+ * File: 4-print_rev.c
+ * Auth: Maurice Kyalo
+ */
+
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	for (i--; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
